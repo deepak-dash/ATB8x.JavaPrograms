570ca0a82d51285817ab5785408ad092c90cb06b
deepak-dash <deepak.dash930@gmail.com>
2024-09-30 21:01:05 +0530

    30th Task and program

---
+++ src/Sept/ex30092024/Lab090.java
@@ -0,0 +1,17 @@
+package Sept.ex30092024;
+
+public class Lab090 {
+    public static void main(String[] args) {
+        // // Functions
+        // Block of code which we can reuse.
+        // 1. Pre-built - Math.max() - Java guys
+        // 2. User created / defined
+
+
+        // Two component of the functions
+        // 1. Define
+        // 2. Call the function
+
+
+    }
+}
---
+++ src/Sept/ex30092024/Lab091.java
@@ -0,0 +1,23 @@
+package Sept.ex30092024;
+
+public class Lab091 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++) {
+            greet();
+        }
+        // Parameter == Arguments
+
+        //  1.Without Parameters and Without Return Type
+        // 2.Without Parameters but With Return Type
+        // 3.With Parameters and Without Return Type
+        // 4. With Parameters and With Return Type
+
+        // Define
+        // No Return Type and No Argument
+        // 1.Without Parameters and Without Return Type
+
+    }
+    public static void greet(){
+        System.out.println("Non return type");
+    }
+}
